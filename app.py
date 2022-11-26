d309427aeb596b03ab7679455255cba733664496
Bbruno Ccoelho <brunoc510@hotmail.com>
2022-11-26 12:16:35 -0300

    App

---
+++ app.py
@@ -0,0 +1,12 @@
+echo "# lab_api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bcoelho92/lab_api.git
+git push -u origin main
+
+git remote add origin https://github.com/bcoelho92/lab_api.git
+git branch -M main
+git push -u origin main
+
